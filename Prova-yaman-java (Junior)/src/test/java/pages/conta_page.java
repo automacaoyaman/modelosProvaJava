40f0a7d0e6a59e4ad418c6a1b3651e657eca2ac4
automacaoyaman <matheus.bezerra@yaman.com.br>
2019-04-04 13:48:41 -0300

    Prova Java junior

---
+++ Prova-yaman-java (Junior)/src/test/java/pages/conta_page.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class conta_page {
+	WebDriver driver;
+	
+	public conta_page(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	
+}
